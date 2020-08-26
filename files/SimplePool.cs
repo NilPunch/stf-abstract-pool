506e8b40b6a2a4b19a8cb3bfb5506265f63ff4ac
Pankevich Daniil <69798762+NilPunch@users.noreply.github.com>
2020-08-26 13:27:42 +0300

    Fill SimplePool.cs

--- files/SimplePool.cs
+++ files/SimplePool.cs
@@ -1,1 +1,14 @@
+public class SimplePool<T> : Pool<T> where T : new()
+{
+	// Called when the pool runs out of objects and needs to create a new one
+	protected override T CreateNewItem() => new T();
 
+	// Called right before getting an object from the pool
+	protected override void ActivateItem(T item) {}
+	
+	// Called right before the object is returned to the pool
+	protected override void DeactivateItem(T item) {}
+	
+	// Since we have not overridden the RefreshCondition method, there is no need to check objects for their freeness
+	protected override bool ItemIsFree(T item) => false;
+}
